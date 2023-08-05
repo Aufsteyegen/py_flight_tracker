f3e29348685a39d529434312353a129a1310aa09
Gregory Caesar <gregmcaesar@gmail.com>
2023-08-04 22:11:17 -0400

    removes backend files from branch

--- backend/flight_tracker/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('sync_flights', views.update_flights, name="update_flights"),
-    path('delete_flight', views.delete_flights, name="delete_flights")
-]
