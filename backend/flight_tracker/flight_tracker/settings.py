9104901dcefa3809c5458b26dbc7c7f3ae81345c
Debian <bitnami@ip-172-26-2-240.ec2.internal>
2023-08-05 12:44:25 +0000

    updates to settings.py to configure for AWS Lightsail (just changed DEBUG value).

--- backend/flight_tracker/flight_tracker/settings.py
+++ backend/flight_tracker/flight_tracker/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['django-env.eba-ysfgwamp.us-west-2.elasticbeanstalk.com']
 
