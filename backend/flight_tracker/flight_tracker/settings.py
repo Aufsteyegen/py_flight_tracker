3104b9bdc42d6d728da05fd8d3cdb13b2688b582
Debian <bitnami@ip-172-26-2-240.ec2.internal>
2023-08-06 02:31:38 +0000

    changes to settings.py to configure for HTTPS support

--- backend/flight_tracker/flight_tracker/settings.py
+++ backend/flight_tracker/flight_tracker/settings.py
@@ -32,6 +32,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['frontend-files.d2784ujtzwur8a.amplifyapp.com/',
                  os.environ.get('LIGHTSAIL_IP')]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CORS_ALLOW_CREDENTIALS = True
 
